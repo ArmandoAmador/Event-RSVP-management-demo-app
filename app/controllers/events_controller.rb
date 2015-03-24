a6452ff400f5ea1b86d8d13076ffefdf8ebe42e6
Armando Amador <arm.amador@gmail.com>
2015-03-23 22:27:10 -0400

    Add authentication to eventstr

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,7 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
-
+  before_filter :authenticate_user!, only: [:new, :edit, :create,
+           :update, :destroy]
   # GET /events
   # GET /events.json
   def index
